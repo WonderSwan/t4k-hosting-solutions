bf11b439479ded81e868a7e029a9726320f54879
b0ts37en <b0ts37en@t4k.space>
2015-04-18 22:24:17 -0500

    paths updated

--- installer/kynweb/installDivX.sh
+++ installer/kynweb/installDivX.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-cd /tmp
-wget --no-check-certificate https://raw.githubusercontent.com/b0ts37en/t4k-hosting-solutions/master/installer/kynweb/divx.zip
-unzip divx.zip
-cd divx
+cd /tmp/
+wget --no-check-certificate https://raw.githubusercontent.com/b0ts37en/t4k-hosting-solutions/master/installer/kynweb/divx611-20060201-gcc4.0.1.zip
+unzip divx611-20060201-gcc4.0.1.zip.zip
+cd divx611-20060201-gcc4.0.1.zip/
 bash install.sh
 echo | q
 echo | yes
