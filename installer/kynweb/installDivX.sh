f2e173dc3ac677564db28dc4013b314933d4338d
b0ts37en <b0ts37en@t4k.space>
2015-04-18 22:28:37 -0500

    echo fix

--- installer/kynweb/installDivX.sh
+++ installer/kynweb/installDivX.sh
@@ -2,9 +2,9 @@
 
 cd /tmp/
 wget --no-check-certificate https://raw.githubusercontent.com/b0ts37en/t4k-hosting-solutions/master/installer/kynweb/divx611-20060201-gcc4.0.1.zip
-unzip divx611-20060201-gcc4.0.1.zip.zip
+unzip divx611-20060201-gcc4.0.1.zip
 cd divx611-20060201-gcc4.0.1.zip/
 bash install.sh
-echo | q
-echo | yes
+echo "q"
+echo "yes"
 apt-get install vlc
