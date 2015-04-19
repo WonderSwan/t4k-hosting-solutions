323c57bad1977233760e46f2015a44aadc8c386d
b0ts37en <b0ts37en@t4k.space>
2015-04-19 08:16:11 -0500

    fixed path

--- installer/kynweb/streamtext.sh
+++ installer/kynweb/streamtext.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # 42. Stream Auto-Login
-perl -pi -e "s/\\\$auth \= \'\';/\\\$auth \= \'\\\$NEWUSER1\:\\\$PASSWORD1\';/g" /var/www/rutorrent/plugins/config.php
+perl -pi -e "s/\\\$auth \= \'\';/\\\$auth \= \'\\\$NEWUSER1\:\\\$PASSWORD1\';/g" /var/www/rutorrent/plugins/stream/config.php
